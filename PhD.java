7815596f8bbe131c36dee79387e2ac7aa13c5bba
Jingyu Yao <yjy.emails@gmail.com>
2020-02-08 23:03:33 -0800

    comments

--- PhD.java
+++ PhD.java
@@ -1,45 +1,48 @@
-/** Bowen Zhang Assignment 1
+/** Bowen Zhang Assignment 1 /// block comments starts on next line
  * An instance maintains infor about the PhD of a person.
  * @author bowenzhang
  *
  */
-
+/// dont use tabs, set convert tab to spaces in your editor
 public class PhD {
-	//Name of the person with a phD, String of length >0
+	//Name of the person with a phD, String of length >0 /// space after double slash
 	private String name;
-	//In range 12 with 1 as January
-	private int month;
+	//In range 12 with 1 as Januar
+	private int month; /// java have native date time and calendar classes, prefer built-in functionality when possible
 	//Year PhD was awarded
 	private int year;
 	//Gender of person. 'g' for girl and 'b' for boy
-	private char gender;
+	private char gender; // prefer creating an enum
+	/// prefer having a List<PhD> advisors; rather than hard coding each
+	/// also, it looks like you are trying to create a double edged graph of advisors, might want to look at
+	/// how graphs are implemented in general
 	//First PhD advisor of this person
 	private PhD advisor1;
 	//Second PhD advisor of this person
 	private PhD advisor2;
 	//Number of PhD advisees of this person
-	private int advisees;
+	private int advisees; /// tracking a list of PhD advisee instance would be better than the raw number
 	
 	/** Constructor: an instance for a person with name n, PhD month m, PhD year
 	and gender g. Its advisors are unknown, and it has no advisees. 
 	Precondition: n has at least 1 char. m is in 1..12. g is 'g'' for girl
 	or 'b' for boy. */
-	public PhD(String n, int m, int y, char g) {
+	public PhD(String n, int m, int y, char g) { /// dont use single letter variable names
 		assert n.length() > 0 && n != null; 
 		assert m > 0 && m < 13;
-		assert g == 'b' | g == 'g';
+		assert g == 'b' | g == 'g'; /// a single | is not 'or' but a 'bit-wise or'
 		name = n;
 		month = m;
 		year = y;
 		gender = g;
-		advisor1 = null;
+		advisor1 = null; /// these can be initialized inline
 		advisor2 = null;
-		advisees = 0; 
-		
+		advisees = 0;
+		/// remove new line
 	}
 	
 	/** Return the name of this person */
-	public String name() { 
+	public String name() { /// java bean convention prefers getName, getMonth, etc
 		return this.name;
 	}
 	
@@ -53,7 +56,7 @@ public class PhD {
 		return this.year;
 	}
 	
-	/** Return the year this person got their PhD */
+	/** Return the year this person got their PhD */  /// copy paste error
 	public boolean isMale() {
 		return this.gender == 'b';
 	}
@@ -71,11 +74,11 @@ public class PhD {
 	/** Return the number of PhD advisees of this person */
 	public int advisees() {
 		return this.advisees;
-	}
+	} /// new line
 	/** Set person's name to newName 
 	 * Precondition: newName has at least one char */
 	public void setName(String newName) {
-		assert newName.length() > 0 && newName != null; 
+		assert newName.length() > 0 && newName != null; /// null assertions should come before member method access
 		this.name = newName;
 	}
 	
@@ -89,7 +92,7 @@ public class PhD {
 	/** Set PhD year to newYear */
 	public void setYear(int newYear) {
 		this.year = newYear;
-	}
+	}/// new line
 	/** Set gender to newGender
 	 * Precondition: newGender is a character 'b' for male and 'g' for female */
 	public void setGender(char newGender) {
@@ -102,7 +105,7 @@ public class PhD {
 	public void addAdvisor1(PhD p) {
 		assert this.advisor1 == null && p != null;
 		this.advisor1 = p;
-		p.advisees = p.advisees + 1;
+		p.advisees = p.advisees + 1; /// directly modifying private member of another object of the same class is frowned upon
 	}
 	
 	/** Add p as the second advisor of this person. 
@@ -115,7 +118,7 @@ public class PhD {
 		this.advisor2 = p; 
 		p.advisees = p.advisees + 1; 
 	}
-	
+	/// group constructors together
 	/** Constructor: a PhD with name n, PhD month m, PhD year y, gender g, first
 	 * advisor adv, and no second advisor
 	 * Precondition: n has at least 1 char, m is in 1..12, g is 'g' for girl or
@@ -171,6 +174,8 @@ public class PhD {
 	 */
 	public boolean arePhDSiblings(PhD p) {
 		assert p != null;
+		/// make a helper function so these arent so stacked, this is also not very scalable
+		/// see comment about using a list
 		return this != p && (
 				(this.advisor1() == p.advisor1() 
 				&& this.advisor1() != null
